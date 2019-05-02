65d3dcb437813406f2a0574f7ffc0a4702348dfd
dewub <wubben.jamie@outlook.com>
2019-05-02 17:36:01 +0200

    fix square marker

--- Marker/CreateMarker.py
+++ Marker/CreateMarker.py
@@ -9,15 +9,11 @@ args = parser.parse_args()
 id = 1 if not args.id else args.id
 
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-img = aruco.drawMarker(aruco_dict, id, 100)
-fig = plt.figure()
-# setting the size to A4 (in inches)
-fig.set_size_inches(11.69, 8.27)
-# A3
-#fig.set_size_inches(16.53,11.69)
+img = aruco.drawMarker(aruco_dict, id, 800)
 
-plt.imshow(img, cmap='gray')
+# good for A4 paper
+plt.figure(figsize=(9.5, 9.5), dpi=100)
+plt.imshow(img, cmap='gray', aspect="equal")
 plt.axis("off")
-plt.savefig("marker" + str(id) + ".pdf", bbox_inches='tight')
+plt.savefig("marker" + str(id) + ".pdf", bbox_inches="tight")
 plt.show()
-
