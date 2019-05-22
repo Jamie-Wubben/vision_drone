b01792d1a81105506f7f9590394ddaab1318c62b
jamie <jamie.wubben@student.uantwerpen.be>
2019-05-22 16:10:26 +0200

    lompe fix

--- Calibration/checkFrameRate.py
+++ Calibration/checkFrameRate.py
@@ -6,7 +6,7 @@ import time
 if __name__ == '__main__':
 
     # Start default camera
-    video = cv2.VideoCapture(0);
+    video = cv2.VideoCapture(0)
 
     # Find OpenCV version
     (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
--- Camera.py
+++ Camera.py
@@ -109,9 +109,9 @@ class Camera:
         self.logger.info("Start camera and record.")
 
         self.running = True
-        self.cap = cv2.VideoCapture("FlightMovies/setup.avi")
+        #self.cap = cv2.VideoCapture("FlightMovies/goodFlight2.avi")
 
-        #self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(0)
         self.logger.info("recording the video feed")
         while self.running:
             self.ret, self.frame = self.cap.read()
